578480716a3c32ac9b66dec1f8b925155433be90
adriantoddross <adriantoddross@gmail.com>
2018-03-01 11:47:21 -0500

    test app display

--- src/components/game.js
+++ src/components/game.js
@@ -1,14 +1,13 @@
 import React from 'react';
-
 import Header from './header';
 import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export class Game extends React.Component {
+export default class Game extends React.Component {
     constructor(props) {
         super(props);
-        
+
         this.state = {};
     }
 
