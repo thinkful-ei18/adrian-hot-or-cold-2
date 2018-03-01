db2047bc77e1c0f1aeacc43a6f93f3c526087f97
adriantoddross <adriantoddross@gmail.com>
2018-03-01 14:14:20 -0500

    convert into class

--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -2,21 +2,32 @@ import React from 'react';
 
 import './guess-form.css';
 
-export default function GuessForm(props) {
-    // store textinput in state and grab it on submit
+export default class GuessForm extends React.Component {
+    // store textinput in state and grab it
 
-    return (
-        <form
-        onSubmit={(e) => {
-            e.preventDefault();
-            props.whenSubmitted();
-        }}
-        >
-            <input onChange={(e) => props.whenAnswered(e.target.value)} type="text" name="userGuess" id="userGuess"
-                className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
-        </form>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                this.props.whenSubmitted();
+            }}
+            >
+                <input onChange={(e) => this.props.whenAnswered(e.target.value)} type="text" name="userGuess" id="userGuess"
+                    className="text" maxLength="3" autoComplete="off"
+                    placeholder="Enter your Guess" required />
+                <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+            </form>
+        );
+    }
+
 };
 
